9491ad1ee7823e0530696397f3aaa30416235017
Kevin <kevin.rodriguez678@gmail.com>
2023-06-02 18:07:43 -0400

    add slice method to obtain apiurl from state

--- src/pages/PokemonDetails/PokemonDetails.jsx
+++ src/pages/PokemonDetails/PokemonDetails.jsx
@@ -1,22 +1,39 @@
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import { getDetails } from "../../services/api-calls";
 
 const PokemonDetails = () => {
   const [pokemonDetails, setPokemonDetails] = useState({})
-  
   const location = useLocation()
 
   useEffect(() => {
-    console.log(location);
-  }, [])
+    // console.log(location.state.pokemonTitle.url.slice(18));
+    const fetchDetails = async () => {
+      const pokemonDetails = await getDetails(location.state.pokemonTitle.url.slice(18))
+      console.log(pokemonDetails);
+      setPokemonDetails(pokemonDetails)
+    }
+    fetchDetails()
+  }, [location.state.pokemonTitle.url])
 
   return (
     <>
       <div>
-        <h3>Pokemon Details</h3>
+        {pokemonDetails.name ? 
+        <>
+          <h2>{pokemonDetails.name.toUpperCase()}</h2>
+          {pokemonDetails.moves.map((move) => (
+            <div key={move.index}>{move.name}</div>
+          ))}
+        </>
+        :
+        <>
+          <p>Loading Pokemon Details...</p>
+        </>
+        }
       </div>
     </>
   );
 }
- 
+
 export default PokemonDetails;
--- src/services/api-calls.js
+++ src/services/api-calls.js
@@ -1,8 +1,7 @@
 const baseUrl = "https://pokeapi.co"
 
 export async function getPokemonList() {
-  const res = await fetch(`${baseUrl}/api/v2/pokemon?limit=100000&offset=0`)
-  console.log(res);
+  const res = await fetch(`${baseUrl}/api/v2/pokemon?limit=493&offset=0`)
   return res.json()
 }
 
