6b5ab807d7d60ed7b358a903c99b8d656e168e92
Kevin <kevin.rodriguez678@gmail.com>
2023-06-09 16:41:41 -0400

    stub up item list page

--- src/pages/ItemList/ItemList.jsx
+++ src/pages/ItemList/ItemList.jsx
@@ -1,9 +1,36 @@
-
+import { useEffect, useState } from "react";
+import { getItemList } from "../../services/api-calls";
+import { Link } from "react-router-dom";
 
 const ItemList = () => {
+  const [itemList, setItemList] = useState([])
+
+  useEffect(() => {
+    const fetchItemList = async () => {
+      const itemData = await getItemList()
+      console.log(itemData);
+      setItemList(itemData.results)
+    }
+    fetchItemList()
+  }, [])
+
   return (
     <>
-      
+      <h3>Item List</h3>
+      {itemList.length ? 
+      <>
+        {itemList.map(item =>
+          <div key={item.index}>
+            <Link to="/item" state={{item}}>{item.name}</Link>
+            <br />
+          </div>
+        )}
+      </>
+      :
+      <>
+        <h4>Loading Helpful Items...</h4>
+      </>
+      }
     </>
   );
 }
